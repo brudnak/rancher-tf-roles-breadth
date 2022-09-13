b970b3384061c069dccdeec1245237beb1f44165
andrew brudnak <brudnak@protonmail.com>
2022-09-13 14:22:34 -0700

    :truck: Breadth role checks are working

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# rancher-tf-roles
+# Rancher Breadth Check Cluster Roles
+
+> Terraform to test performance & functionality of some code in rancher that was checking depth of calls. This is checking to make sure a breadth use case is not affected.
+
+### How to Use
+
+#### What Your `TFVARS` File Should Look Like
+
+This file `terraform.tfvars` should sit next to the `main.tf` file.
+
+🚨 `WARNING!` Make sure `terraform.tfvars` IS NOT TRACKED WITH GIT / UPLOADED TO GitHub 🚨
+
+```tf
+# Variable section
+
+rancher_url       = "enter-your-rancher-url-here"
+rancher_token_key = "enter-your-admin-token-here"
+role_count       = 1100
+```
---
+++ main.tf
@@ -0,0 +1,174 @@
+terraform {
+  required_providers {
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = "1.22.2"
+    }
+  }
+}
+
+provider "rancher2" {
+  api_url   = var.rancher_url
+  token_key = var.rancher_token_key
+  insecure  = true
+}
+
+data "rancher2_role_template" "role_list" {
+  count = var.role_count
+  name = "foo-${count.index+1}"
+  depends_on = [rancher2_role_template.foo]
+}
+
+# Create a new rancher2 cluster Role Template
+resource "rancher2_role_template" "foo" {
+  count = var.role_count
+  name = "foo-${count.index+1}"
+  context = "cluster"
+  default_role = true
+  description = "Terraform role template acceptance test"
+  rules {
+    api_groups = ["*"]
+    resources = ["secrets"]
+    verbs = ["create"]
+  }
+}
+
+locals {
+  dadfish = [for id in data.rancher2_role_template.role_list : id.id]
+}
+
+# Create a new rancher2 cluster Role Template
+resource "rancher2_role_template" "bar" {
+  name = "bar"
+  context = "cluster"
+  default_role = true
+  description = "Terraform role template acceptance test"
+  rules {
+    api_groups = ["*"]
+    resources = ["secrets"]
+    verbs = ["create"]
+  }
+    role_template_ids = local.dadfish
+}
+
+# Variable section
+
+variable "rancher_url" {
+  type        = string
+  description = "URL for Rancher."
+}
+
+variable "rancher_token_key" {
+  type        = string
+  description = "API bearer token for Rancher."
+  sensitive   = true
+}
+
+variable "role_count" {
+  type        = number
+  description = "Amount of roles that you would like to create within Rancher."
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
